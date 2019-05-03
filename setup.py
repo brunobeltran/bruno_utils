76b9268c87a2ed91774bfb56ee8261912cb20a9c
Bruno Beltran <brunobeltran0@gmail.com>
2019-05-03 10:30:04 -0700

    [1.4.6] PyPi version collision, bumping version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
         return f.read()
 
 setup(name='bruno_util',
-      version='1.4.5',
+      version='1.4.6',
       description='Catch-all package for utilities useful to Bruno Beltran',
       long_description=readme(),
       author='Bruno Beltran',
