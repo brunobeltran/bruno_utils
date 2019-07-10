a865da890e2ba7a86aa0c9da03b300ab34f54185
Bruno Beltran <brunobeltran0@gmail.com>
2019-07-09 17:07:38 -0700

    PLOT: easier cmap

--- bruno_util/plotting.py
+++ bruno_util/plotting.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import seaborn as sns
+import numbers
 
 def plot_colored_line(t, x, y, cmap='viridis', linewidth=3, ax=None,
                       colorbar=True):
@@ -36,4 +38,30 @@ def get_lim(x, margin=0.1):
     dx = max - min
     return [min - margin*dx, max + margin*dx]
 
+
+def cmap_from_list(labels, palette=None, log=False, vmin=None, vmax=None):
+    # sequential colormap if numbers
+    if isinstance(labels[0], numbers.Number):
+        labels = np.array(labels)
+        if vmin is None:
+            vmin = labels.min()
+        if vmax is None:
+            vmax = labels.max()
+        if log:
+            cnorm = mpl.colors.LogNorm(vmin=vmin, vmax=vmax)
+        else:
+            cnorm = mpl.colors.Normalize(vmin=vmin, vmax=vmax)
+        if palette is None:
+            palette = 'viridis'
+        cmap = mpl.cm.get_cmap(palette)
+        return lambda l: cmap(cnorm(l))
+    # otherwise categorical map
+    else:
+        if log:
+            raise ValueError('LogNorm makes no sense for categorical labels.')
+        labels = list(set(labels))
+        n_labels = len(labels)
+        pal = sns.color_palette(palette, n_colors=n_labels)
+        cmap = {labels[i]: pal[i] for i in range(n_labels)}
+        return lambda l: cmap[l]
 
