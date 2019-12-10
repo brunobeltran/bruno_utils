218424963cdecb80c6c85d2cd09cbc5535d731f1
Bruno Beltran <brunobeltran0@gmail.com>
2019-12-10 09:21:17 -0800

    CODE: plotting, draw power-law triangle

--- bruno_util/plotting.py
+++ bruno_util/plotting.py
@@ -65,3 +65,102 @@ def cmap_from_list(labels, palette=None, log=False, vmin=None, vmax=None):
         cmap = {labels[i]: pal[i] for i in range(n_labels)}
         return lambda l: cmap[l]
 
+def draw_triangle(alpha, x0, width, orientation, base=10,
+                            **kwargs):
+    """Draw a triangle showing the best-fit slope on a linear scale.
+
+    Parameters
+    ----------
+    alpha : float
+        the slope being demonstrated
+    x0 : (2,) array_like
+        the "left tip" of the triangle, where the hypotenuse starts
+    width : float
+        horizontal size
+    orientation : string
+        'up' or 'down', control which way the triangle's right angle "points"
+    base : float
+        scale "width" for non-base 10
+
+    Returns
+    -------
+    corner : (2,) np.array
+        coordinates of the right-angled corner of the triangle
+    """
+    x0, y0 = x0
+    x1 = x0 + width
+    y1 = y0 + alpha*(x1 - x0)
+    plt.plot([x0, x1], [y0, y1], 'k')
+    if (alpha >= 0 and orientation == 'up') \
+    or (alpha < 0 and orientation == 'down'):
+        plt.plot([x0, x1], [y1, y1], 'k')
+        plt.plot([x0, x0], [y0, y1], 'k')
+        # plt.plot lines have nice rounded caps
+        # plt.hlines(y1, x0, x1, **kwargs)
+        # plt.vlines(x0, y0, y1, **kwargs)
+        corner = [x0, y1]
+    elif (alpha >= 0 and orientation == 'down') \
+    or (alpha < 0 and orientation == 'up'):
+        plt.plot([x0, x1], [y0, y0], 'k')
+        plt.plot([x1, x1], [y0, y1], 'k')
+        # plt.hlines(y0, x0, x1, **kwargs)
+        # plt.vlines(x1, y0, y1, **kwargs)
+        corner = [x1, y0]
+    else:
+        raise ValueError(r"Need $\alpha\in\mathbb{R} and orientation\in{'up', 'down'}")
+    return corner
+
+def draw_power_law_triangle(alpha, x0, width, orientation, base=10,
+                            x0_logscale=True, label=None,
+                            label_padding=0.1, **kwargs):
+    """Draw a triangle showing the best-fit power-law on a log-log scale.
+
+    Parameters
+    ----------
+    alpha : float
+        the power-law slope being demonstrated
+    x0 : (2,) array_like
+        the "left tip" of the power law triangle, where the hypotenuse starts
+        (in log units, to be consistent with draw_triangle)
+    width : float
+        horizontal size in number of major log ticks (default base-10)
+    orientation : string
+        'up' or 'down', control which way the triangle's right angle "points"
+    base : float
+        scale "width" for non-base 10
+
+    Returns
+    -------
+    corner : (2,) np.array
+        coordinates of the right-angled corner of the triangle
+    """
+    if x0_logscale:
+        x0, y0 = [base**x for x in x0]
+    else:
+        x0, y0 = x0
+    x1 = x0*base**width
+    y1 = y0*(x1/x0)**alpha
+    plt.plot([x0, x1], [y0, y1], 'k')
+    if (alpha >= 0 and orientation == 'up') \
+    or (alpha < 0 and orientation == 'down'):
+        plt.plot([x0, x1], [y1, y1], 'k')
+        plt.plot([x0, x0], [y0, y1], 'k')
+        # plt.plot lines have nice rounded caps
+        # plt.hlines(y1, x0, x1, **kwargs)
+        # plt.vlines(x0, y0, y1, **kwargs)
+        corner = [x0, y1]
+    elif (alpha >= 0 and orientation == 'down') \
+    or (alpha < 0 and orientation == 'up'):
+        plt.plot([x0, x1], [y0, y0], 'k')
+        plt.plot([x1, x1], [y0, y1], 'k')
+        # plt.hlines(y0, x0, x1, **kwargs)
+        # plt.vlines(x1, y0, y1, **kwargs)
+        corner = [x1, y0]
+    else:
+        raise ValueError(r"Need $\alpha\in\mathbb{R} and orientation\in{'up', 'down'}")
+    if label is not None:
+        xlabel = x0*base**(width/2)
+        ylabel = y1*base**label_padding if orientation == 'up' else y0*base**(-label_padding)
+        plt.text(xlabel, ylabel, label, horizontalalignment='center',
+                 verticalalignment='center')
+    return corner
